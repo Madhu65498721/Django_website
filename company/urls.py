819580d7f077ad07688f06e7969c21e4a6012839
Madhu.N <162883459+Madhu65498721@users.noreply.github.com>
2024-09-23 15:54:50 +0530

    Update urls.py

--- company/urls.py
+++ company/urls.py
@@ -20,7 +20,7 @@ from app.views import index,contact_form,blog_detail,blogs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', index,name="home"),
+    path('', index,name="home"),
     path('contact/', contact_form ,name="contact_form"),
     path('blog_detail/<blog_id>',blog_detail,name='blog_detail'),
     path('blogs/',blogs,name="blogs"),
